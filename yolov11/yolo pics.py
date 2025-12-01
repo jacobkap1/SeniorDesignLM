0633af5df432883952713e9610d4ae5820d7b95b
Jacob Kaplan <157649147+jacobkap1@users.noreply.github.com>
2025-12-01 09:55:46 -0500

    Enhance code comments for clarity on yolo pics
    Added comments to clarify the purpose of each code section.

--- yolov11/yolo pics.py
+++ yolov11/yolo pics.py
@@ -1,12 +1,16 @@
+# Imports the YOLO class from the Ultralytics library, which contains the tools needed to load and run the YOLO models.
 from ultralytics import YOLO
-
+# Loads the model and runs inference on the input image.
+# This returns a list of YOLO prediction results.
 model = YOLO("yolo11n.pt")
 results = model("thanks.jpg")
-
+# Loop to iterate over each property returned in the results for the input image.
 for result in results:
-    boxes = result.boxes
-    masks = result.masks
-    keypoints = result.keypoints
-    probs = result.probs
-    obb = result.obb
+    boxes = result.boxes   # bounding boxes: give the location and size of an object in the image.
+    masks = result.masks   # segmentation masks: identify exact pixels that belong to an object.
+    keypoints = result.keypoints # keypoints: represent specific landmark positions on an object.
+    probs = result.probs # classification probabilities: represent the model’s confidence for every possible class it can predict.
+    obb = result.obb     # oriented bounding boxes: rotated bounding boxes that can tilt or angle and can follow the actual angle of the object.
+    
+    # Saves the prediction image.
     result.save(filename="result2.jpg")
