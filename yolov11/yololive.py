1c10239e67ba708d4266d601138ba80275053e66
mramos207 <matthewramos325@gmail.com>
2025-12-13 20:38:24 -0500

    yololive.py

--- yolov11/yololive.py
+++ yolov11/yololive.py
@@ -10,9 +10,20 @@ model = YOLO("yolo11n.pt")
 
 #source = 0 is using the default webcam where object detection is ran.
 #show = True displays a window showing webcam frames and bounding boxes.
+#conf = 0.5 is showing a confidence threshold of at least 50%. 
 #results will contain a list of prediction objects, including detected classes and bounding box coordinates.
 
-results = model.predict(source="0", show=True) 
+results = model.predict(source="0", conf=0.5, show=True) 
 
-#Displays YOLO's raw prediction data in the terminal.
-print(results)
+#Displays YOLO's raw prediction data and a text file for each frame, with each row having the format: object class, x center, y center, x width, y width.
+for frame_id, r in enumerate(results):
+    if r.boxes is None:
+        continue
+
+    print(f"\nFrame {frame_id}")
+
+    for box in r.boxes:
+        cls = int(box.cls[0])
+        x, y, w, h = box.xywh[0].tolist() 
+
+        print(f"{cls}, {x:.3f}, {y:.3f}, {w:.3f}, {h:.3f}")
