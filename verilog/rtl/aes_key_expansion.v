3cd1c48d32d8475a6c1803939b0b80fb123c245d
Axiom <101472184+Axiiomatic@users.noreply.github.com>
2025-12-15 11:54:00 -0500

    parametrized shares and replaced sbox with dom-secure version

--- verilog/rtl/aes_key_expansion.v
+++ verilog/rtl/aes_key_expansion.v
@@ -7,25 +7,24 @@
 // - 'done' stays high while new key expansion is not in progress
 // ----------------------------------------------------------------------------- 
 `timescale 1ns/1ps
-module aes256_key_expansion (
+module aes256_key_expansion #(
+    parameter SHARES = 3
+)(
     input  logic         clk,
     input  logic         resetn,
     input  logic         enable_i,
 
     // key inputs
-    input  logic [255:0] k0_i,
-    input  logic [255:0] k1_i,
-    input  logic [255:0] k2_i,
+    input  logic [255:0] k_i[0:SHARES-1],
     input  logic         kvalid_i,
     
-    input  logic [431:0] r_i,
-    input  logic         rvalid_i,
+    // sbox
+    output logic [7:0]   sbox_i[0:SHARES-1],
+    input  logic [7:0]   sbox_o[0:SHARES-1],
 
     // round outputs
     input  logic [3:0]   addr_i,  // round address 0..13
-    output logic [127:0] w0_o,
-    output logic [127:0] w1_o,
-    output logic [127:0] w2_o,
+    output logic [127:0] w_o[0:SHARES-1],
     output logic         valid_o
 );
     int i;
@@ -41,39 +40,36 @@ module aes256_key_expansion (
     endfunction
 
     // ------------------------------------------------ Registers ------------------------------------------------
-    logic [31:0] w0[0:59], w1[0:59], w2[0:59];
+    logic [31:0] w[0:SHARES-1][0:59], w_tmp[0:SHARES-1];
     logic [5:0]  t;
+    logic [2:0]  p;
+    logic [1:0]  idx_i, idx_o;
     
-    logic [31:0] sw_w0, sw_w1, sw_w2;
-    logic [31:0] subword0, subword1, subword2;
+    logic [7:0]  word_i[0:SHARES-1][0:3], word_o[0:SHARES-1][0:2];
+    logic [7:0]  x[0:SHARES-1];
+    logic [7:0]  s[0:SHARES-1];
     
     // ------------------------------------------- Next/Derived States -------------------------------------------
-    subword sw (
-        .w0(sw_w0),
-        .w1(sw_w1),
-        .w2(sw_w2),
-        .r(r_i[431:0]),
-        .sw0(subword0),
-        .sw1(subword1),
-        .sw2(subword2)
-    );
+    assign sbox_i = x;
+    assign s = sbox_o;
     
     // --------------------------------------------------- FSM ---------------------------------------------------
-    typedef enum logic [2:0] {LOAD, COMPUTE_1, COMPUTE_2, WAIT_END} state_t;
+    typedef enum logic [2:0] {LOAD, COMPUTE_1, SUBWORD, COMPUTE_2, WAIT_END} state_t;
     state_t state;
 
     // ------------------------------------------ Main Sequential Block ------------------------------------------
     always_ff @(posedge clk or negedge resetn) begin
         if (!resetn) begin          
-            w0 <= '{default:32'd0};
-            w1 <= '{default:32'd0};
-            w2 <= '{default:32'd0};
-            
-            subword0 <= 32'd0;
-            subword1 <= 32'd0;
-            subword2 <= 32'd0;
+            w      <= '{default:'{default:32'd0}};
+            word_i <= '{default:'{default:8'd0}};
+            word_o <= '{default:'{default:8'd0}};
+            x      <= '{default:8'd0};
             
             t     <= 6'd8;
+            p     <= 3'd0;
+            idx_i <= 2'd0;
+            idx_o <= 2'd0;
+            
             state <= LOAD;
         end else if (enable_i) begin          
             case (state)
@@ -81,51 +77,78 @@ module aes256_key_expansion (
                     t <= 6'd8;
                     
                     if (kvalid_i) begin
-                        {w0[0], w0[1], w0[2], w0[3], w0[4], w0[5], w0[6], w0[7]} <= k0_i ^ k1_i ^ k2_i;
-                        {w1[0], w1[1], w1[2], w1[3], w1[4], w1[5], w1[6], w1[7]} <= k1_i;
-                        {w2[0], w2[1], w2[2], w2[3], w2[4], w2[5], w2[6], w2[7]} <= k2_i;
+                        for (i = 0; i < SHARES; i++) begin
+                            {w[i][0], w[i][1], w[i][2], w[i][3], w[i][4], w[i][5], w[i][6], w[i][7]} <= k_i[i];
+                        end
 
                         state <= COMPUTE_1;
                     end
                 end
                 
                 COMPUTE_1: begin
-                    
                     if (t[2:0] == 3'd0) begin
-                        if (rvalid_i) begin
-                            sw_w0 = RotWord(w0[t-1]);
-                            sw_w1 = RotWord(w1[t-1]);
-                            sw_w2 = RotWord(w2[t-1]);
+                        for (i = 0; i < SHARES; i++) begin
+                            {word_i[i][3], word_i[i][2], word_i[i][1], word_i[i][0]} = RotWord(w[i][t-1]);
                             
-                            state   <= COMPUTE_2;
+                            x[i] = word_i[i][0];
                         end
+                        
+                        p     <= 3'd0;
+                        idx_i <= 2'd1;
+                        idx_o <= 2'd0;
+                        state <= SUBWORD;
                     end else if (t[2:0] == 3'd4) begin
-                        if (rvalid_i) begin
-                            sw_w0 = w0[t-1];
-                            sw_w1 = w1[t-1];
-                            sw_w2 = w2[t-1];
+                        for (i = 0; i < SHARES; i++) begin
+                            {word_i[i][3], word_i[i][2], word_i[i][1], word_i[i][0]} = w[i][t-1];
                             
-                            state   <= COMPUTE_2;
+                            x[i] = word_i[i][0];
                         end
+                        
+                        p     <= 3'd0;
+                        idx_i <= 2'd1;
+                        idx_o <= 2'd0;
+                        state <= SUBWORD;
                     end else begin
-                        w0[t] <= w0[t-8] ^ w0[t-1];
-                        w1[t] <= w1[t-8] ^ w1[t-1];
-                        w2[t] <= w2[t-8] ^ w2[t-1];
+                        for (i = 0; i < SHARES; i++) begin
+                            w[i][t] <= w[i][t-8] ^ w[i][t-1]; 
+                        end
                         
                         if (t >= 6'd59) state <= WAIT_END;
                         else t <= t + 1;
                     end
                 end
                 
+                SUBWORD: begin
+                    if (idx_i > 0) begin
+                        for (i = 0; i < SHARES; i++) begin
+                            x[i] = word_i[i][idx_i];
+                        end
+                        idx_i++;
+                    end
+                    
+                    if (p == 3'd7) begin
+                        for (i = 0; i < SHARES; i++) begin
+                            word_o[i][idx_o] <= s[i];
+                        end
+                        
+                        if (idx_o >= 2'd2) state <= COMPUTE_2;
+                        else idx_o++;
+                    end else p++;
+                end
+                
                 COMPUTE_2: begin
-                    if (t[2:0] == 3'd0) begin 
-                        w0[t] <= w0[t-8] ^ subword0 ^ Rcon[t >> 3];
-                        w1[t] <= w1[t-8] ^ subword1 ^ Rcon[t >> 3];
-                        w2[t] <= w2[t-8] ^ subword2 ^ Rcon[t >> 3];
+                    for (i = 0; i < SHARES; i++) begin
+                        w_tmp[i] = {s[i], word_o[i][2], word_o[i][1], word_o[i][0]};
+                    end
+                    
+                    if (t[2:0] == 3'd0) begin
+                        for (i = 0; i < SHARES; i++) begin
+                            w[i][t] <= w[i][t-8] ^ w_tmp[i] ^ Rcon[t >> 3];
+                        end
                     end else begin
-                        w0[t] <= w0[t-8] ^ subword0;
-                        w1[t] <= w1[t-8] ^ subword1;
-                        w2[t] <= w2[t-8] ^ subword2;
+                        for (i = 0; i < SHARES; i++) begin
+                            w[i][t] <= w[i][t-8] ^ w_tmp[i];
+                        end
                     end
                     
                     state <= COMPUTE_1;
@@ -136,9 +159,9 @@ module aes256_key_expansion (
                     t <= 6'd8;
                     
                     if (kvalid_i) begin
-                        {w0[0], w0[1], w0[2], w0[3], w0[4], w0[5], w0[6], w0[7]} <= k0_i;
-                        {w1[0], w1[1], w1[2], w1[3], w1[4], w1[5], w1[6], w1[7]} <= k1_i;
-                        {w2[0], w2[1], w2[2], w2[3], w2[4], w2[5], w2[6], w2[7]} <= k2_i;
+                        for (i = 0; i < SHARES; i++) begin
+                            {w[i][0], w[i][1], w[i][2], w[i][3], w[i][4], w[i][5], w[i][6], w[i][7]} <= k_i[i];
+                        end
 
                         state <= COMPUTE_1;
                     end
@@ -149,8 +172,12 @@ module aes256_key_expansion (
         end
     end
         // ------------------------------------------------- Outputs -------------------------------------------------
-    assign w0_o = {w0[4*addr_i], w0[4*addr_i+1], w0[4*addr_i+2], w0[4*addr_i+3]};
-    assign w1_o = {w1[4*addr_i], w1[4*addr_i+1], w1[4*addr_i+2], w1[4*addr_i+3]};
-    assign w2_o = {w2[4*addr_i], w2[4*addr_i+1], w2[4*addr_i+2], w2[4*addr_i+3]};
+    genvar j;
+    generate
+        for (j = 0; j < SHARES; j++) begin
+            assign w_o[j] = {w[j][4*addr_i], w[j][4*addr_i+1], w[j][4*addr_i+2], w[j][4*addr_i+3]};
+        end
+    endgenerate
+    
     assign valid_o = (state == WAIT_END);
 endmodule
