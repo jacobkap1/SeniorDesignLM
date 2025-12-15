5d12e699306e7df908a61f061226d6b681d218b1
Axiom <101472184+Axiiomatic@users.noreply.github.com>
2025-12-15 11:55:56 -0500

    added proper rng calls for sbox

--- verilog/rtl/rng.v
+++ verilog/rtl/rng.v
@@ -1,51 +1,54 @@
 `timescale 1ns/1ps
 
-module rng(
-    input  logic          clk,
-    input  logic          resetn,
-    input  logic          enable_i,
-    input  logic [127:0]  seed_i,
+module aes_rng #(
+    parameter SHARES = 3
+)(
+    input  logic         clk,
+    input  logic         resetn,
+    input  logic         enable_i,
+    input  logic [127:0] seed_i,
 
-    output logic [255:0]  m0_o,
-    output logic [255:0]  m1_o,
-    output logic [431:0]  rw_o,
-    output logic [3455:0] rb_o,
-    output logic          valid_o
+    output logic [255:0] m[0:SHARES-2],
+    output logic [3:0]   zm0[0:(SHARES*(SHARES-1)/2)-1],
+    output logic [3:0]   zm1[0:(SHARES*(SHARES-1)/2)-1],
+    output logic [3:0]   zm2[0:(SHARES*(SHARES-1)/2)-1],
+    output logic [1:0]   zi0[0:(SHARES*(SHARES-1)/2)-1],
+    output logic [1:0]   zi1[0:(SHARES*(SHARES-1)/2)-1],
+    output logic [1:0]   zi2[0:(SHARES*(SHARES-1)/2)-1],
+    output logic         valid_o
 );
-
+    int i;
     // Optional: seed the RNG once at reset using SystemVerilog's srandom()
     initial begin
         void'($urandom(seed_i));  // seeds SV RNG
     end
-    
-    logic [3455:0] randomness;
-    
-    assign m0_o = randomness[255:0];
-    assign m1_o = randomness[511:256];
-    assign rw_o = randomness[943:512];
-    assign rb_o = randomness;
 
     always_ff @(posedge clk or negedge resetn) begin
         if (!resetn) begin
-            randomness  <= '0;
+            m   <= '{default: 256'd0};
+            
+            zm0 <= '{default: 4'd0};
+            zm1 <= '{default: 4'd0};
+            zm2 <= '{default: 4'd0};
+            
+            zi0 <= '{default: 2'd0};
+            zi1 <= '{default: 2'd0};
+            zi2 <= '{default: 2'd0};
+            
             valid_o <= 1'b0;
         end else if (enable_i) begin
-            randomness <= {
-                $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(),
-                $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(),
-                $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(),
-                $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(),
-                $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(),
-                $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(),
-                $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(),
-                $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(),
-                $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(),
-                $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(),
-                $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(),
-                $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(),
-                $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(),
-                $urandom(), $urandom(), $urandom(), $urandom()
-            };
+            for (i = 0; i < SHARES; i++) begin
+                // sim only, need to update with proper rng
+                m[i] <= {$urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom(), $urandom()};
+                
+                zm0[i] <= $urandom();
+                zm1[i] <= $urandom();
+                zm2[i] <= $urandom();
+                
+                zi0[i] <= $urandom();
+                zi1[i] <= $urandom();
+                zi2[i] <= $urandom();
+            end
             
             valid_o <= 1'b1;
         end else begin
