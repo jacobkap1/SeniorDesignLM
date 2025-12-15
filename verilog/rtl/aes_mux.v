48dad055164f4ccc10165c5830548d83f99c512d
Axiom <101472184+Axiiomatic@users.noreply.github.com>
2025-12-15 11:54:41 -0500

    moved masking to external component

--- verilog/rtl/aes_mux.v
+++ verilog/rtl/aes_mux.v
@@ -1,51 +1,38 @@
-module aes_mux(
-    input  wire [127:0] w0_i,
-    input  wire [127:0] w1_i,
-    input  wire [127:0] w2_i,
-    input  wire         wvalid_i,
+module aes_mux #(
+    parameter SHARES = 3
+)(
+    input  logic [127:0] w_i[0:SHARES-1],
+    input  logic         wvalid_i,
     
-    input  wire [127:0] iv_i,
-    input  wire         ivalid_i,
+    input  logic [127:0] iv_i[0:SHARES-1],
+    input  logic         ivalid_i,
     
-    input  wire [127:0] c_i,
-    input  wire         cvalid_i,
+    input  logic [127:0] c_i[0:SHARES-1],
+    input  logic         cvalid_i,
     
-    input  wire [255:0] m_i,
-    input  wire         mvalid_i,
-    
-    input  wire [1:0]   sel_i,
-    output reg  [127:0] out0_o,
-    output reg  [127:0] out1_o,
-    output reg  [127:0] out2_o,
-    output reg          valid_o
+    input  logic [1:0]   sel_i,
+    output logic [127:0] d_o[0:SHARES-1],
+    output logic         valid_o
 );
     always_comb begin
         case (sel_i)
             2'd0: begin
-                out0_o   = w0_i;
-                out1_o   = w1_i;
-                out2_o   = w2_i;
-                valid_o  = wvalid_i;
+                d_o     = w_i;
+                valid_o = wvalid_i;
             end 
             
             2'd1: begin
-                out0_o  = iv_i;
-                out1_o  = m_i[255:128];
-                out2_o  = m_i[127:  0];
-                valid_o = ivalid_i & mvalid_i;
+                d_o     = iv_i;
+                valid_o = ivalid_i;
             end
             
             2'd2: begin
-                out0_o  = c_i;
-                out1_o  = m_i[255:128];
-                out2_o  = m_i[127:  0];
-                valid_o = cvalid_i & mvalid_i;
+                d_o     = c_i;
+                valid_o = cvalid_i;
             end
             
             default: begin
-                out0_o  = 128'd0;
-                out1_o  = 128'd0;
-                out2_o  = 128'd0;
+                d_o     = '{default:128'd0};
                 valid_o = 1'b0;
             end
         endcase
