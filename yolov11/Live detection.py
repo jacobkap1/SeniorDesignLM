7edf21e11a344a097c246ba5978edc50afbfe88a
mramos207 <matthewramos325@gmail.com>
2026-04-27 00:44:54 -0400

    Live detection.py

--- yolov11/Live detection.py
+++ yolov11/Live detection.py
@@ -11,20 +11,20 @@ Frame_Dir = "data/live_frames"
 Label_Dir = "data/live_labels"
 Save_Dir = "data/live_saved"
 
-os.makedirs(Save_Dir, exist_ok = True)
 os.makedirs(Frame_Dir, exist_ok = True)
 os.makedirs(Label_Dir, exist_ok = True)
+os.makedirs(Save_Dir, exist_ok = True)
 
 model_coco = YOLO("yolo11n.pt")
-model_pen = YOLO("pen/runs/detect/trains/weights/best.pt")
+model_pen = YOLO("pen.pt")
 
-Pen_ID = {0: 80}
+pen_ID = {0: 80}
 
 def iou(b1, b2):
-    x1_min, y1_min = b1[1]-b1[3]/2, b1[2]-b1[4]/2
-    x1_max, y1_max = b1[1]+b1[3]/2, b1[2]+b1[4]/2
-    x2_min, y2_min = b2[1]-b2[3]/2, b2[2]-b2[4]/2
-    x2_max, y2_max = b2[1]+b2[3]/2, b2[2]+b2[4]/2
+    x1_min, y1_min = b1[1] - b1[3] / 2, b1[2] - b1[4] / 2
+    x1_max, y1_max = b1[1] + b1[3] / 2, b1[2] + b1[4] / 2
+    x2_min, y2_min = b2[1] - b2[3] / 2, b2[2] - b2[4] / 2
+    x2_max, y2_max = b2[1] + b2[3] / 2, b2[2] + b2[4] / 2
 
     inter_w = max(0, min(x1_max, x2_max) - max(x1_min, x2_min))
     inter_h = max(0, min(y1_max, y2_max) - max(y1_min, y2_min))
@@ -46,7 +46,7 @@ while cap.isOpened():
     if not ret:
         break
 
-    frame_name = f"frame{frame_id:d}.jpg"
+    frame_name = f"frame{frame_id: d}.jpg"
     frame_path = os.path.join(Frame_Dir, frame_name)
     cv2.imwrite(frame_path, frame)
 
@@ -85,9 +85,10 @@ while cap.isOpened():
         for r in results_pen:
             if not r.boxes:
                 continue
+
             for box in r.boxes:
-                cls_model = int(box.cls[0])
-                cls = Pen_ID.get(cls_model, cls_model)
+                pen_cls = int(box.cls[0])
+                cls = pen_ID.get(pen_cls, pen_cls)
 
                 x, y, w, h = box.xywhn[0].tolist()
                 conf = float(box.conf[0])
@@ -103,8 +104,8 @@ while cap.isOpened():
                 y_true.append(label)
                 y_scores.append(conf)
 
-    saved = results_coco[0].plot(show = False)
-    saved = results_pen[0].plot(img = saved, show = False)
+    saved = results_coco[0].plot()
+    saved = results_pen[0].plot(img = saved)
     saved_path = os.path.join(Save_Dir, frame_name)
 
     cv2.imwrite(saved_path, saved) 
@@ -127,3 +128,7 @@ if len(set(y_true)) > 1:
 
     y_pred = (np.array(y_scores) >= CONF_Threshold).astype(int)
     print(f"F1: {f1_score(y_true, y_pred):.3f}")
+
+
+
+
